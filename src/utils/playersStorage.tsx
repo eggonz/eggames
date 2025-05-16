beed0f1dfeaad8d588395e1bb2c108c2e9eac61d
eggonz <egoitz.gonz@gmail.com>
2025-05-16 14:08:34 +0200

    use Players type in playersStorage

--- src/utils/playersStorage.tsx
+++ src/utils/playersStorage.tsx
@@ -1,12 +1,29 @@
-// Utility functions for localStorage
+/**
+ * Utility functions for managing player data in localStorage.
+ *
+ * This file provides functions to store, retrieve, count, and clear player data
+ * from the browser's localStorage. It uses a predefined storage key to manage
+ * the data.
+ *
+ * Functions:
+ * - `getStoredPlayers`: Retrieves the list of stored players from localStorage.
+ * - `storePlayers`: Stores a list of players in localStorage.
+ * - `getStoredPlayersCount`: Returns the count of stored players.
+ * - `clearPlayers`: Clears all stored player data from localStorage.
+ */
+
+import type { Player } from "../types/Player.tsx";
+
+// Constants
 const STORAGE_KEY = 'gamePlayersList';
 
+// Functions
 const getStoredPlayers = () => {
   const storedPlayers = localStorage.getItem(STORAGE_KEY);
   return storedPlayers ? JSON.parse(storedPlayers) : [];
 };
 
-const storePlayers = (players) => {
+const storePlayers = (players: Player[]) => {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(players));
 };
 
